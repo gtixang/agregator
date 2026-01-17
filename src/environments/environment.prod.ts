3ea0e6850ac2f546e8e9d9a6673d5f056100183a
Runar Nazmutdinov <github.runar@gmail.com>
2026-01-17 09:01:45 +0300

    chore: add environment files for production build

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://liztczorploivglizsvy.supabase.co',
+  supabaseKey: 'sb_publishable_BH4fGH0wc5DErlTt44L65Q_MEAkwy2c',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://liztczorploivglizsvy.supabase.co',
+  supabaseKey: 'sb_publishable_BH4fGH0wc5DErlTt44L65Q_MEAkwy2c',
+};
